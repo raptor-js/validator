a0e8dbf3aa153407a1411d3844afcfd98ec59b85
Bri Ward <me@briward.com>
2026-02-17 18:02:35 +0000

    Add types.d.ts example back into the codebase.

---
+++ types.d.ts
@@ -0,0 +1,15 @@
+import type { StandardSchemaV1 } from "./src/types/standard-schema-v1.ts";
+
+declare global {
+  interface Request {
+    validate<T>(
+      validator: StandardSchemaV1<T, any>,
+    ): Promise<T>;
+
+    validateSafe<T>(
+      validator: StandardSchemaV1<T, any>,
+    ): Promise<T>;
+  }
+}
+
+export {};
